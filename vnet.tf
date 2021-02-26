d6c93c1aa273e1d6cd5d077f3c39f430f375da59
rory283 <roryjohnston893@gmail.com>
2021-02-26 16:55:48 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/roryjohnston-training/network/azurerm"
   version = "3.0.1"
-  resource_group_name = "rorvy283rg"
+  resource_group_name = "network"
 }
